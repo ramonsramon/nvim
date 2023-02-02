563d56c2da134f566ff195347aab6272fc51ddab
Ramon Ramon <ramonsramon24@gmail.com>
2023-02-01 20:41:07 -0600

    Change keymap for opening file in new vertical tab

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,16 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<C-p>", builtin.find_files)
 vim.keymap.set("n", "<leader>l", builtin.live_grep)
+vim.keymap.set("n", "<leader>f", builtin.grep_string)
+
+require("telescope").setup {
+pickers = {
+  find_files = {
+    mappings = {
+      i = {
+        ["<C-o>"] = require('telescope.actions').select_vertical
+      },
+    },
+  },
+},
+}
